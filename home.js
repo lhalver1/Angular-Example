3ad3ca472e1d21f93ee32fb9f42d1ec4239a3cdd
Logan Halverson <lhalvey09@gmail.com>
2016-07-25 10:19:05 -0700

    Added home.js as main driver for index.html updated index.html

---
+++ home.js
@@ -0,0 +1,37 @@
+var myApp = angular.module('myApp',[]);
+
+//myApp.directive('myDirective', function() {});
+//myApp.factory('myService', function() {});
+
+function MyCtrl($scope) {
+    $scope.name = 'Superhero';
+    $scope.editing = false;
+    $scope.themeName = "bs3";
+    $scope.selectedRow = null;
+    $scope.rows = generateRows(Math.round(Math.random() * (5 - 1) + 1));
+    
+    
+    $scope.selectTableRow = function(row) {
+    	if($scope.selectedRow == row) {
+      	$scope.selectedRow = null;
+      } else {
+      	$scope.selectedRow = row;
+      }
+    }
+    $scope.editName = function() {
+    	$scope.editing = true;
+    }
+    function generateRows(x) {
+    	var rows = [],j=x;
+      
+      for(var i = 0; i < x ; i++) {
+      	rows.push({
+        	col1: "Data"+i,
+          col2: "Data"+j
+        });
+        j--;
+      }
+      
+    	return rows;
+    }
+}
