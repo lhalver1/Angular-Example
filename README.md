b713fb2b5f00b66d6896c2ff859dd02a68860a54
Logan Halverson <lhalvey09@gmail.com>
2016-07-26 08:11:00 -0700

    Updated Readme and fixed formatting issues with homeCtrl

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 This is my learning Git Repo
 
 It is a simple Twitter Bootstrap 3 and Twitter Bootstrap 4 version switcher.
-The user may swap the current version of Bootstrap dynamically to view the
+The user can swap the current version of Bootstrap dynamically, via radio buttons, to view the
 differences live.
--- js/homeController.js
+++ js/homeController.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp',[]);
+var myApp = angular.module('myApp', []);
 
 //myApp.directive('myDirective', function() {});
 //myApp.factory('myService', function() {});
@@ -9,29 +9,31 @@ myApp.controller('MyCtrl', function MyCtrl($scope) {
     $scope.themeName = "bs3";
     $scope.selectedRow = null;
     $scope.rows = generateRows(Math.round(Math.random() * (5 - 1) + 1));
-    
-    
-    $scope.selectTableRow = function(row) {
-    	if($scope.selectedRow == row) {
-      	$scope.selectedRow = null;
-      } else {
-      	$scope.selectedRow = row;
-      }
+
+
+    $scope.selectTableRow = function (row) {
+        if ($scope.selectedRow == row) {
+            $scope.selectedRow = null;
+        } else {
+            $scope.selectedRow = row;
+        }
     }
-    $scope.editName = function() {
-    	$scope.editing = true;
+
+    $scope.editName = function () {
+        $scope.editing = true;
     }
+
     function generateRows(x) {
-    	var rows = [],j=x;
-      
-      for(var i = 0; i < x ; i++) {
-      	rows.push({
-        	col1: "Data"+i,
-          col2: "Data"+j
-        });
-        j--;
-      }
-      
-    	return rows;
+        var rows = [], j = x;
+
+        for (var i = 0; i < x; i++) {
+            rows.push({
+                col1: "Data" + i,
+                col2: "Data" + j
+            });
+            j--;
+        }
+
+        return rows;
     }
 });
