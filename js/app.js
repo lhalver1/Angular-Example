a31daff0c07a3cc2e6331127761b883224c27e37
Logan Halverson <lhalvey09@gmail.com>
2016-08-05 10:43:57 -0700

    auto focus for editable fields

--- js/app.js
+++ js/app.js
@@ -17,3 +17,32 @@ myApp.config(function($routeProvider) {
         })
 
 });
+
+myApp.directive('inputFocus', function($timeout, $parse) {
+  return {
+    //scope: true,   // optionally create a child scope
+    link: function(scope, element, attrs) {
+      var model = $parse(attrs.inputFocus);
+      scope.$watch(model, function(value) {
+        if(value === true) { 
+          $timeout(function() {
+            element[0].focus();
+
+            if (element[0].setSelectionRange) {
+                var len = element[0].value.length;
+                element[0].setSelectionRange(len, len);
+            }
+            else {
+                element[0].value = element[0].value;
+            }
+
+          });
+        }
+      });
+      // on blur event:
+      element.bind('blur', function() {
+         scope.$apply(model.assign(scope, false));
+      });
+    }
+  };
+});
